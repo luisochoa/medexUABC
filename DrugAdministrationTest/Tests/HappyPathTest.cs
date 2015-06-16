59ff8dff8f0ab0687fdce9d667dc932a222c497e
Renteria Zules Jorge Luis <luis.renteria@uabc.edu.mx>
2015-06-16 13:45:03 -0700

    first method

--- DrugAdministrationTest/Tests/HappyPathTest.cs
+++ DrugAdministrationTest/Tests/HappyPathTest.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace DrugAdministrationTest
 {
+    [TestFixture]
     public class HappyPathTest
     {
-
+        [Test]
+        public void ShouldReturnDateNotNull() { 
+            
+        }
     }
 }
