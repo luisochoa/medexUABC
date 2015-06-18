3d63acb5d40f114c5d4fd0ed2ac840413b40e4ee
Luis <ph.lwyz@gmail.com>
2015-06-18 11:27:21 -0700

    Sadpath changed

--- DrugAdministrationTest/Tests/SadPathTest.cs
+++ DrugAdministrationTest/Tests/SadPathTest.cs
@@ -12,24 +12,51 @@ namespace DrugAdministrationTest
     {
 
         [Test]
-        public void ShouldReturnDateNull()
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Should_Throw_NullReferenceException_In_Date()
         {
             //Disponer
             var sut = new ValuesController();
 
             //Actuar
             var result = DateTime.Today;  // se espera que retorne un null metodo de obtener fecha
-            DateTime? expected = null;
-
-            //Declarar
-            Assert.AreEqual(expected, result);
+            //sut.PrintDate();
 
         }
 
         [Test]
-        public void ShouldReturnDateYearFieldNull()
+        public void Should_Return_Null_Date()
         {
             var sut = new ValuesController();
+
+            var result = DateTime.Today;
+            DateTime? expected = null;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Should_Return_Date_Fields_Null()
+        {
+            var sut = new ValuesController();
+
+            var year = DateTime.Today.Year; // El values controller retorna un null
+            var month = DateTime.Today.Month;
+            var day = DateTime.Today.Day;
+
+            DateTime? expected = null;
+
+            Assert.AreEqual(expected, year);
+            Assert.AreEqual(expected, month);
+            Assert.AreEqual(expected, day);
+            
+        }
+
+        [Test]
+        public void Should_Return_Date_Without_DateTime_Type()
+        {
+            var sut = new ValuesController();
+
 
         }
       
