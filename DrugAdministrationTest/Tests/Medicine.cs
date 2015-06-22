e06b782c276ae50a373efc170997e47f1b45407f
Renteria Zules Jorge Luis <luis.renteria@uabc.edu.mx>
2015-06-22 12:49:09 -0700

    commit

---
+++ DrugAdministrationTest/Tests/Medicine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrugAdministrationTest.Tests
+{
+    class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
