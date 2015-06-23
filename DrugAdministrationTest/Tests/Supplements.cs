d1050c586d2769276e0d630b8e6bd6fdc49a91ce
Renteria Zules Jorge Luis <luis.renteria@uabc.edu.mx>
2015-06-23 11:07:45 -0700

    New class Supplements

---
+++ DrugAdministrationTest/Tests/Supplements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrugAdministrationTest.Tests
+{
+    class Supplements
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
